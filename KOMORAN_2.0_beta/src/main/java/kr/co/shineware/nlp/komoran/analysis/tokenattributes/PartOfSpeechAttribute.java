c4724c3bba58bc2be9430ded0d6b7b7a8475ba51
jong03 <jong03.choi@gmail.com>
2015-03-26 14:02:28 +0900

    rebase

--- KOMORAN_2.0_beta/src/main/java/kr/co/shineware/nlp/komoran/analysis/tokenattributes/PartOfSpeechAttribute.java
+++
@@ -1,9 +0,0 @@
-package kr.co.shineware.nlp.komoran.analysis.tokenattributes;
-
-import org.apache.lucene.util.Attribute;
-
-public interface PartOfSpeechAttribute extends Attribute {
-	public String partOfSpeech();
-
-	public void setPartOfSpeech(String partOfSpeech);
-}
--- KOMORAN_2.0_beta/src/main/java/kr/co/shineware/nlp/komoran/analysis/tokenattributes/SemanticClassAttribute.java
+++
@@ -1,9 +0,0 @@
-package kr.co.shineware.nlp.komoran.analysis.tokenattributes;
-
-import org.apache.lucene.util.Attribute;
-
-public interface SemanticClassAttribute extends Attribute {
-	public String semanticClass();
-
-	public void setSemanticClass(String semanticClass);
-}
